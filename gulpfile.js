86c4ee9c2625e76dc7eacb10a66fc43411c19fa7
Stephen Shaw <stephens@webactiv.co.uk>
2015-06-04 18:35:59 +0100

    Updated SASS & Compass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
+
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
     gulp.src('components/coffee/tagline.coffee')
@@ -16,5 +19,18 @@ gulp.task('scripts', function() {
   return gulp.src('./components/scripts/*.js')
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('build/development/js'));
+    .pipe(gulp.dest('build/development/js'))
+});
+
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+  .pipe(compass({
+      sass: 'components/sass',
+      image: 'build/development/images',
+      style: 'expanded'
+  }))
+    .pipe(concat('script.js'))
+    .on('error', gutil.log)
+    .pipe(gulp.dest('build/development/js'))
 });

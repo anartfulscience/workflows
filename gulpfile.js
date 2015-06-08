c262da4f4f409c912f8825f1c62e0d3823eee5da
Stephen Shaw <stephens@webactiv.co.uk>
2015-06-08 14:40:35 +0100

    Compress html

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -82,7 +83,9 @@ gulp.task('connect', function () {
 });
 
 gulp.task('html', function () {
-    gulp.src(htmlSources)
+    gulp.src('build/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload())
 
 });
@@ -96,7 +99,7 @@ gulp.task('json', function () {
 gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['scripts']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('build/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
     gulp.watch('components/sass/*.scss', ['compass']);
 });

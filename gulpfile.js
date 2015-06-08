3e6951e20f893ca80ccf531a05439fdcff190b59
Stephen Shaw <stephens@webactiv.co.uk>
2015-06-08 15:43:20 +0100

    Added image compression

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
     jsonminify = require('gulp-jsonminify'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
     concat = require('gulp-concat');
 
 var env,
@@ -91,6 +93,17 @@ gulp.task('html', function () {
 
 });
 
+gulp.task('images', function() {
+  gulp.src('build/development/images/**/*.*')
+    .pipe(gulpif(env === 'production', imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngcrush()]
+    })))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+    .pipe(connect.reload())
+});
+
 gulp.task('json', function () {
     gulp.src('build/development/js/*.json')
     .pipe(gulpif(env === 'production', jsonminify()))
@@ -104,7 +117,9 @@ gulp.task('watch', function () {
     gulp.watch(jsSources, ['scripts']);
     gulp.watch('build/development/*.html', ['html']);
     gulp.watch('build/development/js/*.json', ['json']);
+    gulp.watch('build/development/images/**/*.*', ['images']);
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'scripts', 'compass', 'connect', 'watch']);
+
+gulp.task('default', ['html', 'json', 'coffee', 'scripts', 'compass', 'images', 'connect', 'watch']);

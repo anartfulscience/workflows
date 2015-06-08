6bc67673d78351b3cc90a94c91b7e1901c0d0d58
Stephen Shaw <stephens@webactiv.co.uk>
2015-06-08 15:11:23 +0100

    Added json compression

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 var env,
@@ -91,7 +92,9 @@ gulp.task('html', function () {
 });
 
 gulp.task('json', function () {
-    gulp.src(jsonSources)
+    gulp.src('build/development/js/*.json')
+    .pipe(gulpif(env === 'production', jsonminify()))
+    .pipe(gulpif(env === 'production', gulp.dest('build/production/js/')))
         .pipe(connect.reload())
 
 });
@@ -100,7 +103,7 @@ gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['scripts']);
     gulp.watch('build/development/*.html', ['html']);
-    gulp.watch(jsonSources, ['json']);
+    gulp.watch('build/development/js/*.json', ['json']);
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
